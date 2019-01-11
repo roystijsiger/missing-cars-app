86795d1af96a7b71b156f21784f3f042f05b08c8
DESKTOP-Q3BTUMG\royst <Roy@delcom.nl>
2019-01-11 23:42:33 +0100

    Navigation to new activity added.

--- app/src/main/java/com/whereismyvehicle/android/whereismyvehicle/Views/MainActivity.java
+++ app/src/main/java/com/whereismyvehicle/android/whereismyvehicle/Views/MainActivity.java
@@ -1,5 +1,6 @@
 package com.whereismyvehicle.android.whereismyvehicle.Views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.whereismyvehicle.android.whereismyvehicle.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,18 +83,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_missing_vehicles_index) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent missingVehicleIndex = new Intent(this, VehicleActivity.class);
+            startActivity(missingVehicleIndex);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/whereismyvehicle/android/whereismyvehicle/Views/VehicleActivity.java
@@ -0,0 +1,31 @@
+package com.whereismyvehicle.android.whereismyvehicle.Views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.whereismyvehicle.android.whereismyvehicle.R;
+
+public class VehicleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
