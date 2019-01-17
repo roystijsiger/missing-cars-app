a2409185794928fe0f2a890ee4cad727377f3c3e
Andreas Furster <andreas.furster@hotmail.com>
2019-01-17 21:57:49 +0100

    Added basic communication between Fragment, Presenter and Proxy

--- app/src/main/java/com/wherismyvehicle/whereismyvehicle/Presenters/SightingsFragmentPresenter.java
+++ app/src/main/java/com/wherismyvehicle/whereismyvehicle/Presenters/SightingsFragmentPresenter.java
@@ -1,5 +1,7 @@
 package com.wherismyvehicle.whereismyvehicle.Presenters;
 
+import com.wherismyvehicle.whereismyvehicle.Data.CachedDataPersistance;
+import com.wherismyvehicle.whereismyvehicle.Data.DataPersinstance;
 import com.wherismyvehicle.whereismyvehicle.Models.Sighting;
 
 import java.util.ArrayList;
@@ -7,15 +9,20 @@ import java.util.ArrayList;
 public class SightingsFragmentPresenter {
     private ArrayList<Sighting> sightings;
     private View view;
+    private DataPersinstance<Sighting> dataPersinstance;
 
     public SightingsFragmentPresenter(View view) {
         this.view = view;
 
-        // TODO: Get sightings from api/storage
-        sightings = new ArrayList<>();
+        dataPersinstance = new CachedDataPersistance();
+    }
+
+    public void LoadSightings() {
+        ArrayList<Sighting> sightings = dataPersinstance.FetchAll();
+        view.ShowSightings(sightings);
     }
 
     public interface View {
-
+        void ShowSightings(ArrayList<Sighting> sightings);
     }
 }
--- app/src/main/java/com/wherismyvehicle/whereismyvehicle/Views/SightingsFragment.java
+++ app/src/main/java/com/wherismyvehicle/whereismyvehicle/Views/SightingsFragment.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.wherismyvehicle.whereismyvehicle.Models.Sighting;
 import com.wherismyvehicle.whereismyvehicle.Presenters.SightingsFragmentPresenter;
+
+import java.util.ArrayList;
 
 public class SightingsFragment extends Fragment implements SightingsFragmentPresenter.View {
     private SightingsFragmentPresenter presenter;
@@ -16,13 +19,19 @@ public class SightingsFragment extends Fragment implements SightingsFragmentPres
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         this.presenter = new SightingsFragmentPresenter(this);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        presenter.LoadSightings();
+
         return inflater.inflate(R.layout.fragment_sightings, container, false);
+    }
+
+    @Override
+    public void ShowSightings(ArrayList<Sighting> sightings) {
+       // TODO Roy: Add this arraylist to an listview.
     }
 }
