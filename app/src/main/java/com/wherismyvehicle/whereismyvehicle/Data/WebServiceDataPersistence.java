77b55845ae880958835c05512a31f5cd0497118c
Andreas Furster <andreas.furster@hotmail.com>
2019-01-20 00:13:49 +0100

    Implementing other http client methods

--- app/src/main/java/com/wherismyvehicle/whereismyvehicle/Data/WebServiceDataPersistence.java
+++ app/src/main/java/com/wherismyvehicle/whereismyvehicle/Data/WebServiceDataPersistence.java
@@ -1,9 +1,19 @@
 package com.wherismyvehicle.whereismyvehicle.Data;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.google.gson.Gson;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class WebServiceDataPersistence<T> implements DataPersistence<T> {
+
+    public static final MediaType JSON_CONTENT_TYPE = MediaType.parse("application/json; charset=utf-8");
 
     private final OkHttpClient client;
     private final String baseUrl;
@@ -15,9 +25,8 @@ public class WebServiceDataPersistence<T> implements DataPersistence<T> {
 
     @Override
     public DataPersistenceAction<T> Fetch(Class serializationClass, Object id) {
-        Request request = new Request.Builder()
-                .url(baseUrl.concat("/").concat(id.toString()))
-                .build();
+        Request request = createRequest(baseUrl.concat("/").concat(id.toString()))
+            .build();
 
         DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
 
@@ -28,9 +37,8 @@ public class WebServiceDataPersistence<T> implements DataPersistence<T> {
 
     @Override
     public DataPersistenceAction<T[]> FetchAll(Class serializationClass) {
-        Request request = new Request.Builder()
-                .url(baseUrl)
-                .build();
+        Request request = createRequest(baseUrl)
+            .build();
 
         DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
 
@@ -41,26 +49,77 @@ public class WebServiceDataPersistence<T> implements DataPersistence<T> {
 
     @Override
     public DataPersistenceAction<T> Insert(Class serializationClass, T object) {
-        throw new UnsupportedOperationException();
+        String json = new Gson().toJson(object);
+
+        Request request = createRequest(baseUrl)
+            .post(RequestBody.create(JSON_CONTENT_TYPE, json))
+            .build();
+
+        DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
+
+        client.newCall(request).enqueue(task);
+
+        return task;
     }
 
     @Override
     public DataPersistenceAction<T[]> Insert(Class serializationClass, T[] object) {
-        throw new UnsupportedOperationException();
+        String json = new Gson().toJson(object);
+
+        Request request = createRequest(baseUrl)
+                .post(RequestBody.create(JSON_CONTENT_TYPE, json))
+                .build();
+
+        DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
+
+        client.newCall(request).enqueue(task);
+
+        return task;
     }
 
     @Override
     public DataPersistenceAction<T> Update(Class serializationClass, T object) {
-        throw new UnsupportedOperationException();
+        String json = new Gson().toJson(object);
+
+        Request request = createRequest(baseUrl)
+                .patch(RequestBody.create(JSON_CONTENT_TYPE, json))
+                .build();
+
+        DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
+
+        client.newCall(request).enqueue(task);
+
+        return task;
     }
 
     @Override
     public DataPersistenceAction Delete(Class serializationClass, Object id) {
-        throw new UnsupportedOperationException();
+        Request request = createRequest(baseUrl.concat(id.toString()))
+                .build();
+
+        DataPersistenceHttpAction task = new DataPersistenceHttpAction(serializationClass);
+
+        client.newCall(request).enqueue(task);
+
+        return task;
     }
 
     @Override
     public DataPersistenceAction DeleteAll(Class serializationClass) {
         throw new UnsupportedOperationException("Deleting all entities is not supported on the webservice");
+    }
+
+    private Request.Builder createRequest(String endpoint) {
+        return new Request.Builder()
+            .url(endpoint)
+            .addHeader("Authentication", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InN0cmluZ0BleGFtcGxlLmNvbSIsIm5iZiI6MTU0NzkzNjQzMywiZXhwIjoxNTc5NDcyNDMzLCJpYXQiOjE1NDc5MzY0MzN9.1YINBnLk4S_kv434UjCLIogrDNAMqAOgczhReytz2Os");
+    }
+
+    private boolean connectionAvailable(){
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
