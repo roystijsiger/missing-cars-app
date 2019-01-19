a0558fc3f1b5ac3490835a95e191cd19553258ac
DESKTOP-Q3BTUMG\royst <Roy@delcom.nl>
2019-01-20 00:25:53 +0100

    add: Location is now correctly in thel is item, clicking the geo icon will open google maps on the spot

--- app/src/main/java/com/wherismyvehicle/whereismyvehicle/Views/Adapters/VehiclesAdapter.java
+++ app/src/main/java/com/wherismyvehicle/whereismyvehicle/Views/Adapters/VehiclesAdapter.java
@@ -1,16 +1,20 @@
 package com.wherismyvehicle.whereismyvehicle.Views.Adapters;
 
         import android.content.Context;
+        import android.content.Intent;
+        import android.net.Uri;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.ArrayAdapter;
+        import android.widget.ImageButton;
         import android.widget.TextView;
 
         import com.wherismyvehicle.whereismyvehicle.Models.Vehicle;
         import com.wherismyvehicle.whereismyvehicle.Views.R;
 
         import java.util.ArrayList;
+        import java.util.Locale;
 
 public class VehiclesAdapter extends ArrayAdapter<Vehicle> {
     public VehiclesAdapter(Context context, ArrayList<Vehicle> vehicles) {
@@ -18,7 +22,7 @@ public class VehiclesAdapter extends ArrayAdapter<Vehicle> {
     }
 
     public View getView(int position, View convertView, ViewGroup parent){
-        Vehicle vehicle = getItem(position);
+        final Vehicle vehicle = getItem(position);
 
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.vehicle_simple_list_item, parent, false);
@@ -28,11 +32,26 @@ public class VehiclesAdapter extends ArrayAdapter<Vehicle> {
         TextView vehicleType = convertView.findViewById(R.id.txt_vehicle_type);
         TextView vehicleLicense = convertView.findViewById(R.id.txt_vehicle_license);
         TextView vehicleColor = convertView.findViewById(R.id.txt_vehicle_color);
+        ImageButton showLocationButton = convertView.findViewById(R.id.btn_show_location);
+
+        showLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", vehicle.getLocation().getLatitude(), vehicle.getLocation().getLongitude());
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                getContext().startActivity(intent);
+            }
+        });
+
 
         vehicleBrand.setText(vehicle.getBrand());
         vehicleColor.setText(vehicle.getColor());
-        vehicleLicense.setText(vehicle.getLicencePlate());
-//        vehicleType.setText(vehicle.getType().toString());
+        vehicleLicense.setText(vehicle.getLicensePlate());
+
+        if(vehicle.getType() != null){
+            vehicleType.setText(vehicle.getType());
+        }
+
 
         return convertView;
     }
